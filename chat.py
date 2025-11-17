ccae54de08596309ad4291b0dc4be23aa23d3860
adbella <ajh86463447@gmail.com>
2025-11-17 13:34:48 +0900

    knowledge graph web update

--- chat.py
+++ chat.py
@@ -73,10 +73,3 @@ def auto_knowledge_graph(context):
 
     categorized_data = graphcategory(parsing_output)
     return categorized_data
-
-context = """지도 학습이란 레이블(Label) 이라는 정답과 함께 학습하는 것을 말한다. 자연어 처리는 대부분 지도 학습에 속한다. 레이블이라는 말 이외에도y, 실제값 등으로 부르기도 한다. 
-간단히 말해 선생님이 문제를 내고 그 다음 바로 정답까지 같이 알려주는 방식의 학습 방법이다. 
-여러 문제와 답을 같이 학습함으로 미지의 문제에 대한 올바른 답을 예측하고자 하는 방법이다. 
-지도학습을 위한 데이터로는 문제와 함께 그 정답까지 같이 알고 잇는 데이터가 선택된다."""
-
-auto_knowledge_graph(context)
